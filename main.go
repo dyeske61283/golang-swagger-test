3b87e997fa1706a84c8162de003d11c4b6206666
Kevin Dyes <kevin.dyes@icloud.com>
2023-10-18 21:17:15 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+
+	docs "golang-swagger/docs"
+
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func main() {
+	r := gin.Default()
+	docs.SwaggerInfo.BasePath = ""
+	r.GET("/ping", ping)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+type ReturnModel struct {
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	Property int    `json:"property"`
+}
+
+// PingExample godoc
+//
+//	@Summary	ping example
+//	@Schemes
+//	@Description	do ping
+//	@Tags			example
+//	@Accept			json
+//	@Produce		json
+//	@Success		200	{object} ReturnModel "ok"
+//	@Router			/ping [get]
+func ping(c *gin.Context) {
+	resp := ReturnModel{Id: "2241441", Name: "wfffqw", Property: 12244555}
+	c.JSON(http.StatusOK,
+		&resp)
+}
